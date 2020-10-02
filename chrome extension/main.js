3fff3d2b76a1f17bbb5ceebb7f4b7d7fbea26413
Xiao Yu Omeara <63607193+xyomeara@users.noreply.github.com>
2020-10-01 22:55:11 -0400

    10/01/2020

--- chrome extension/main.js
+++ chrome extension/main.js
@@ -1,1 +1,35 @@
-${".text"}.css({"background-color": "yellow", "font-size": "200%"})
+$("#button").click(function(){
+  let count = 30;
+  $("#timer").text(count);
+  $("#message").text("Timer Active!")
+
+  const set = setInterval(function() {
+    count -= 1;
+    $("#timer").text(count);
+
+    if(count === 0) {
+      clearInterval(set);
+      $("#message").text("get up and move!!!!!!")
+
+
+
+
+
+      
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+  }, 200);
+});

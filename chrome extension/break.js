e2909d2849308a1e6e64f592433d0bddfaa5c409
Chandni Patel <Chandni@chandni-patels-macbook.local>
2020-10-03 10:47:30 -0400

    Added motivation photo

--- chrome extension/break.js
+++ chrome extension/break.js
@@ -1,3 +1,21 @@
+const images = [
+    "motivationimages/mot1.png",
+    "motivationimages/mot1.png",
+    "motivationimages/mot1.png",
+    "motivationimages/mot1.png",
+    "motivationimages/mot1.png",
+    "motivationimages/mot1.png",
+    "motivationimages/mot1.png",
+    "motivationimages/mot1.png",
+    "motivationimages/mot1.png",
+    "motivationimages/mot1.png",
+    "motivationimages/mot1.png",
+    "motivationimages/mot1.png",
+    
+  ]
+
+const idx = [Math.floor(Math.random() * 12)];
+
 console.log("JS working?!?")
 let count2 = 5;
  $("#break-timer").text(count2);
@@ -9,6 +27,7 @@ function countDown (){
     if(count2 <0){
         $("#break-timer").toggleClass( "turnRed");
         $("#break-message").text("Back to work!");
+        $("#picture").attr("src", `${images[idx]}`)
     } else {
     $("#break-timer").text(count2);
     }
